01bf0451cc703583d1f4a562268b11c6c5a7562d
Krishna Kumar Mandal <mandalkrishna739@gmail.com>
2025-07-28 22:53:40 +0545

    login, logout middleare functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,17 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function login()
     {
+        // $password = Hash::make('password123');
+        // dd($password);
         return view('auth.login');
+    }
+
+    public function login_post(Request $request)
+    {
+        
+        // $credentials = $request->validate([
+        //     'email' => 'required|email',
+        //     'password' => 'required'
+        // ]);
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password], true)) {
+            if (Auth::user()->is_role == 'ADM') {
+                return to_route('dashboard');
+            } else {
+                return redirect()->back()->with('error', 'Please enter correct credentials!');
+            }
+        } else {
+            return redirect()->back()->with('error', 'Please enter correct credentials!');
+        }
     }
 
     public function forgot()
     {
         return view('auth.forgot');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return to_route('login');
     }
 
     public function register()
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,13 @@ use Illuminate\Support\Facades\Route;
 
 // Auth Routes
 Route::get('/', [AuthController::class, 'login'])->name('login');
+Route::post('login_post', [AuthController::class, 'login_post'])->name('login_post');
 Route::get('forgot', [AuthController::class, 'forgot'])->name('forgot');
 Route::get('register', [AuthController::class, 'register'])->name('register');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
 
 // Admin Dashboard
-Route::get('admin/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+Route::middleware(['admin'])->group(function () {
+    Route::get('admin/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+});
